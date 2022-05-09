ad4d25b2095654cf5ddc0ddf42d247f98f8ad168
Apvivar92@gmail.com <Apvivar92@gmail.com>
2022-05-09 14:32:34 -0700

    completed debugg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import Nav from './components/Nav';
+import ContentWrapper from './components/ContentWrapper';
+import Footer from './components/Footer';
+import { useSelector, useDispatch, connect } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
-  
+  const dispatch = useDispatch();
+  const mode = useSelector((state) => state.mode);
+
+  const toggleMode = () => {
+    mode.darkMode ? dispatch(lightMode()) : dispatch(darkMode());
+  };
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div
+      style={{ backgroundColor: mode.color1, color: mode.color }}
+      className='App'
+    >
       <Nav />
       <ContentWrapper />
       <Footer />
